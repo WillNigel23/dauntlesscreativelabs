9b3bcb7abeafbde6c657d23509cf02f11bbb7a8b
WillNigel23 <wcdejesus@up.edu.ph>
2023-07-30 21:12:18 +0800

    Navbar sticky animation on scroll

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,10 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  static targets = ["menuIcon", "hiddenMenu"]
+  static targets = ["menuIcon", "hiddenMenu", "interface"];
+  static values = { scrolled: Boolean };
 
   connect() {
-    this.isOpen = false;    
+    this.isOpen = false;
+    this.previousScrollY = 0;
+    window.addEventListener('scroll', this.onScroll.bind(this));
+  }
+
+  disconnect() {
+    window.removeEventListener('scroll', this.onScroll.bind(this));
   }
 
   toggle() {
@@ -25,5 +32,17 @@ export default class extends Controller {
   closeMenu() {
     this.menuIconTarget.classList.remove("open");
     this.hiddenMenuTarget.classList.remove("open");
+  }
+
+  onScroll() {
+    const scrollY = window.scrollY;
+
+    if (scrollY > 30 && !this.scrolledValue) {
+      this.interfaceTarget.classList.remove("show");
+      this.scrolledValue = true;
+    } else if (scrollY <= 30 && this.scrolledValue) {
+      this.interfaceTarget.classList.add("show");
+      this.scrolledValue = false;
+    }
   }
 }
